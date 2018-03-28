5ca087c8e97355c503a8a759f47f04e4db765dcd
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2018-03-28 11:45:01 +0300

    Push datalayer vars

--- gtm-custom-data-layer-variables.php
+++ gtm-custom-data-layer-variables.php
@@ -3,6 +3,7 @@
 Plugin Name:    GTM custom data layer variables
 Plugin URI:     https://github.com/Intraktio/gtm-custom-data-layer-variables
 Description:    Add custom data layer variables for Google Tag Manager. Requires Meta Box and MB Fieldset Any plugins.
+		MB Fieldset Any plugin: https://github.com/karrirasinmaki/mb-fieldset-any
 Version:        20180327
 Author:         Intraktio Ltd
 Author URI:     https://intraktio.com
@@ -10,16 +11,10 @@ License:        MIT
 Domain Path:    /languages
  */
 
-if ( !class_exists( 'RWMB_Field' ) ) {
-  echo __('Meta Box plugin required.', 'gtm_custom_data_layer_variables');
-}
-if (!class_exists( 'RWMB_Fieldset_Any_Field' ) ) {
-  echo __('MB Fieldset Any plugin required.', 'gtm_custom_data_layer_variables');
-}
-
 class GTM_Custom_Data_Layer_Variables {
   function init() {
     add_action( 'rwmb_meta_boxes', array($this, 'init_meta_box'), 10 );
+    add_action( 'wp_head', array($this, 'data_layer_markup'), 10 );
   }
 
   function init_meta_box( $meta_boxes ) {
@@ -57,6 +52,35 @@ class GTM_Custom_Data_Layer_Variables {
 
     return $meta_boxes;
   }
+
+  function data_layer_markup() {
+    $variables = array();
+    $meta = rwmb_meta( 'gtm_data_layer_variables' );
+    foreach( $meta as $key=>&$val ) {
+      if (empty($val['key'])) {
+	continue;
+      }
+      $variables[$val['key']] = $val['value'];
+    }
+?>
+<script data-cfasync="false" type="text/javascript">
+    var dataLayer = dataLayer || [];
+    dataLayer.push(<?php echo json_encode($variables); ?>);
+</script>
+<?php
+  }
 }
 
-(new GTM_Custom_Data_Layer_Variables())->init();
+function init_gtm_custom_data_layer_variables() {
+  if ( !class_exists( 'RWMB_Field' ) ) {
+    echo __('Meta Box plugin required.', 'gtm_custom_data_layer_variables');
+  }
+  if (!class_exists( 'RWMB_Fieldset_Any_Field' ) ) {
+    echo __('MB Fieldset Any plugin required.', 'gtm_custom_data_layer_variables');
+  }
+  else {
+    (new GTM_Custom_Data_Layer_Variables())->init();
+  }
+}
+
+add_action( 'init', 'init_gtm_custom_data_layer_variables', 11 );
